268d8d87547e7087ed60c2dfebcdc1b7e7bda687
Kainos <gwkainos@gmail.com>
2025-01-06 17:09:07 +0900

    Test: 테스트 코드 한줄 작성 - 실패하는 코드 작성

--- src/test/java/kr/kainos/tdd/purchase/PurchaseTest.java
+++ src/test/java/kr/kainos/tdd/purchase/PurchaseTest.java
@@ -17,16 +17,24 @@ public class PurchaseTest {
   @Test
   public void generateLottoNumbers() throws Exception {
     List<Integer> lottoNumbers = createLottoNumbers();
-    assertEquals(6, lottoNumbers.size(), "로또 번호는 6개여야 합니다.");
-    assertTrue(lottoNumbers.stream().allMatch(num -> num >= 1 && num <= 45),
-            "로또 번호는 1~45 입니다.");
-    assertEquals(6, Set.copyOf(lottoNumbers).size(), "로또 번호는 중복되지 않아야 합니다.");
+    assertEquals(6, lottoNumbers.size());
+    assertTrue(lottoNumbers.stream().allMatch(num -> num >= 1 && num <= 45));
+    assertEquals(6, Set.copyOf(lottoNumbers).size());
   }
 
   @Test
   public void purchaseLotto_insufficientMoney() throws Exception {
     LottoPurchaseResult lottoPurchaseResult = purchaseLotto(2000, 3);
     assertEquals("금액이 부족합니다.", lottoPurchaseResult.getMessage());
+  }
+
+  @Test
+  public void purchaseLotto_exactAmount() throws Exception {
+    LottoPurchaseResult lottoPurchaseResult = purchaseLotto(3000, 3);
+
+    assertEquals(3, lottoPurchaseResult.getLotto().size());
+    assertEquals(0, lottoPurchaseResult.getMoney());
+    assertEquals("3장이 발행 되었습니다", lottoPurchaseResult.getMessage());
   }
 
   private LottoPurchaseResult purchaseLotto(int totalMoney, int gameCount) {
