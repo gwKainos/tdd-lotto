5f4ff57fb4c3183c1e9dee10fb1f254fe93b7e18
Kainos <gwkainos@gmail.com>
2025-01-06 15:44:13 +0900

    Test: 테스트 코드 한줄 작성 - 실패하는 코드 작성

--- src/test/java/kr/kainos/tdd/purchase/PurchaseTest.java
+++ src/test/java/kr/kainos/tdd/purchase/PurchaseTest.java
@@ -1,10 +1,16 @@
 package kr.kainos.tdd.purchase;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class PurchaseTest {
 
   @Test
-  public void nothing() throws Exception {
+  public void generateLottoNumbers() throws Exception {
+    List<Integer> lottoNumbers = createLottoNumbers();
+    assertEquals(6, lottoNumbers.size());
   }
 }
